b8e9a934538c6aa73cb957f76f0f420d671a972e
Tejas Nanaware <Tejas-Nanaware@users.noreply.github.com>
2017-02-18 19:31:39 +0530

    Update registerValidation.php

--- registerValidation.php
+++ registerValidation.php
@@ -1,7 +1,7 @@
 <?php 
 	$captcha_response = $_POST['g-recaptcha-response'];
-	$secret_key = "6LeshBUUAAAAAC7SiwFP0M1cW-OfVqxXHza0rXmc";
-	if (isset($captcha_response) && $captcha_response) {
+	$secret_key = "";
+                if (isset($captcha_response) && $captcha_response) {
 		// echo $captcha_response;
 		$ip = $_SERVER['REMOTE_ADDR'];
 		$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret_key&response=$captcha_response&remoteip=$ip");
